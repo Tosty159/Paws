37033cdd356e58a2255427acf25ca70a8e4433d6
Tosty159 <jadey6125@proton.me>
2026-05-17 14:21:32 +0100

    Get source file.

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello from Paws!\n");
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s file.paws\n", argv[0]);
+		return 1;
+	}
+
+	char *filename = argv[1];
+	FILE *fp = fopen(filename, "r");
+	if (!fp) {
+		fprintf(stderr, "Error opening %s\n", filename);
+
+		return 1;
+	}
+
+	return 0;
 }
